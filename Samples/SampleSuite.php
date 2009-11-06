24cb918ece1ea8e7f467fbb678c100bdd016e045
asandman2 <asandman2@5d4b435c-4d46-d949-aab2-e601ca53af14>
2009-11-06 22:03:33 +0000

    git-svn-id: svn://doctor/SpiraTeam%20Add%20Ons/Trunk@9341 5d4b435c-4d46-d949-aab2-e601ca53af14

--- Samples/SampleSuite.php
+++ Samples/SampleSuite.php
@@ -3,25 +3,34 @@
  * Passes a list of tests to be executed to PHPUnit and adds the custom SpiraTest Listener
  * 
  * @author		Inflectra Corporation
- * @version		2.2.0
+ * @version		2.3.0
  *
  */
  
  require_once 'PHPUnit/Framework.php';
  require_once 'PHPUnit/TextUI/ResultPrinter.php';
  require_once './SimpleTest.php';
- //require_once 'SimpleTestListener.php';
+ require_once '../SpiraListener/Listener.php';
  
  // Create a test suite that contains the tests
  // from the ArrayTest class
   $suite = new PHPUnit_Framework_TestSuite('SimpleTest');
  
- // Create a test result and attach a SimpleTestListener
- // object as an observer to it.
+ //Create a new SpiraTest listener instance and specify the connection info
+ $spiraListener = new SpiraListener_Listener;
+ $spiraListener->baseUrl = 'http://localhost/SpiraTest';
+ $spiraListener->userName = 'fredbloggs';
+ $spiraListener->password = 'fredbloggs';
+ $spiraListener->projectId = 1;
+ $spiraListener->releaseId = 1;
+ $spiraListener->testSetId = 1;
+ 
+ // Create a test result and attach the SpiraTest listener
+ // object as an observer to it (as well as the default console text listener)
  $result = new PHPUnit_Framework_TestResult;
  $textPrinter = new PHPUnit_TextUI_ResultPrinter;
  $result->addListener($textPrinter);
- //$result->addListener(new SimpleTestListener);
+ $result->addListener($spiraListener);
  
  // Run the tests and print the results
  $result = $suite->run($result);
